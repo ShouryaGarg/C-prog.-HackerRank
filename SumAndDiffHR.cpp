120b1ad3d642e5563b007ae28ea18727f8c320d7
ShouryaGarg <77744661+ShouryaGarg@users.noreply.github.com>
2021-03-03 21:02:01 +0530

    Update SumAndDiffHR.cpp

--- SumAndDiffHR.cpp
+++ SumAndDiffHR.cpp
@@ -3,8 +3,10 @@ int main()
 {
     int a ,b;
     float x ,y;
+    
     scanf("%d %d" ,&a ,&b);
     scanf("%f %f" ,&x ,&y);
+    
     printf("%d %d" ,a+b ,a-b);
     printf("\n%.1f %.1f" ,x+y ,x-y);   
         
